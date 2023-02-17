6f6e3c1bb72bebc740ba70eee0d7451d1f147f19
williammleziva <willmlez@gmail.com>
2023-02-17 01:29:01 -0600

    side bar, work in progress

--- server.py
+++ server.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
