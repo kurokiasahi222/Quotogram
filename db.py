fc98942073cd4c63ee2f459cde45c3889db28d1e
not-anwaar <anwaar.hadi@gmail.com>
2023-02-20 12:37:00 -0600

    api endpoints

--- db.py
+++ db.py
@@ -47,28 +47,46 @@ def get_db_cursor(commit=False):
           cursor.close()
 
 # TODO: Delete this once we have actual queries
-def test_db_connection():
-    with get_db_cursor() as cur:
-        cur.execute("SELECT 1=1")
-        return cur.fetchall()
+# def test_db_connection():
+#     with get_db_cursor() as cur:
+#         cur.execute("SELECT 1=1")
+#         return cur.fetchall()
 
-def get_posts(user_id, q=ALL_POSTS):
+def get_posts(uid, q=ALL_POSTS):
     # make a SELECT query
-    q = q.format(user_id)
+    q = q.format(user_id=uid)
     with get_db_cursor() as cur:
         current_app.logger.info("Executing query {}".format(q))
         cur.execute(q)
         return cur.fetchall()
-
-def get_post_table_json():
-    #jsonify the post table
-    with get_db_cursor() as cur:
-        cur.execute("select row_to_json(post) from post")
-        return cur.fetchall()
     
+def get_table_json(table_name="post"):
+    #jsonify a table from the database
+    with get_db_cursor() as cur:
+        cur.execute("select row_to_json({name}) from {name}".format(name=table_name))
+        # cur.execute("select row_to_json(%s) from %s", (table_name, table_name))
+        return cur.fetchall()
+
 def get_posts_by_category(category,q=POSTS_BY_CATEGORY):
     q = q.format(post_category=category) 
     with get_db_cursor() as cur:
         current_app.logger.info("Executing query {}".format(q))
         cur.execute(q)
         return cur.fetchall()
+
+if __name__ == "__main__":
+    # tests for get_posts, get_posts_by_category, get_post_table_json, and get_post_category_json
+    app = Flask(__name__)
+    with app.app_context():
+        setup()
+        # get_posts tests
+        # print(get_posts("quotagram"))
+        # print(get_posts("idk"))
+        # # get_posts_by_category tests
+        # print(get_posts_by_category("inspirational"))
+        # print(get_posts_by_category("idk"))
+        # json tests
+        print(get_table_json())
+        print(get_table_json("board"))
+        print(get_table_json("followers"))
+        print(get_table_json("post_category"))
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import *
 from db import *
 
 app = Flask(__name__)
@@ -11,3 +11,12 @@ def init():
 def hello_world():
     print(test_db_connection()) # TODO: remove later
     return render_template("index.html")
+
+@app.route('/api') #default api route jsonifies post table
+def default_table():
+    return get_table_json('post')
+
+@app.route('/api/<table_name>') #need to specify a table name here in the route
+def table(table_name='post'):
+    table_name = str(table_name)
+    return get_table_json(table_name)
