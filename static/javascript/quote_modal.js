e3c9ca8e4c8474e8f92822f66143c66294e069f7
Jason Woitalla <jason.woitalla@gmail.com>
2023-02-23 12:42:03 -0600

    Quote modal updates correctly, buttons make api fetch calls

--- static/javascript/quote_modal.js
+++ static/javascript/quote_modal.js
@@ -8,12 +8,18 @@ function displayQuoteModal(quoteId) {
     let quoteContext = document.getElementById("quote-context-" + quoteId).innerText;
     let userLink = document.getElementById("user-link-" + quoteId).href;
     let userLinkText = document.getElementById("user-link-" + quoteId).innerText;
+    let userPic = document.getElementById("user-picture-" + quoteId).src;    
+    let likeCount = document.getElementById("quote-like-count-" + quoteId).innerText;
+    let datePosted = document.getElementById("quote-date-posted-" + quoteId).innerText;
 
     document.getElementById("quote-text-modal").innerText = quoteText;
     document.getElementById("quote-author-modal").innerText = quoteAuthor;
     document.getElementById("quote-context-modal").innerText = quoteContext;
     document.getElementById("user-link-modal").href = userLink;
     document.getElementById("user-link-modal").innerText = userLinkText;
+    document.getElementById("user-picture-modal").src = userPic;
+    document.getElementById("quote-like-count-modal").innerText = likeCount;
+    document.getElementById("quote-date-posted-modal").innerText = datePosted;
     MicroModal.show("modal-1", {
         disableScroll: true,
     });
