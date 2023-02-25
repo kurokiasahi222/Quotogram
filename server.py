d8cb12bc8a1fd5094e4e6009fa9ad76e5dbec4f8
Aditi Patil <aditivpatil5@gmail.com>
2023-02-24 21:41:03 -0600

    modified server.py with minor organizational changes and removing print statements

--- server.py
+++ server.py
@@ -46,12 +46,9 @@ def index():
         user = session['user']
         res = get_posts_logged_in(session["uid"]) 
         posts = json.dumps(res)  # convert result to json string
-        print(posts)
     else:
-        # TODO: Get posts from non-logged in version
         res = get_posts_not_logged_in()
         posts = json.dumps(res)  # convert result to json string
-        # print(posts)
     return render_template("index.html", user=user, posts=posts)
 
 @app.route("/profile")
@@ -91,7 +88,20 @@ def like_quote(quote_id):
             return jsonify({"status": "failed"})
     else:
         abort(401) # send back an 401 Unauthorized message
-    
+
+@app.route("/explore")
+def explore():
+    return render_template("explore.html")
+
+@app.route("/new_post", methods=["POST"])
+def new_post():
+    user_id = session.get("uid", "jakdghjgdshJHBshjqUAs") # can change the default uid later
+    quote = request.form.get("quote", "NOT FILLED OUT")
+    quote_author = request.form.get("quote_author", "NOT FILLED OUT")
+    context = request.form.get("context", "NOT FILLED OUT")
+    add_post(user_id, quote, quote_author, context)
+    return redirect("/")
+  
 ######### Auth0 stuff ########
 
 @app.route("/login")
@@ -134,16 +144,3 @@ def logout():
             quote_via=quote_plus,
         )
     )
-
-@app.route("/explore")
-def explore():
-    return render_template("explore.html")
-
-@app.route("/new_post", methods=["POST"])
-def new_post():
-    user_id = session.get("uid", "jakdghjgdshJHBshjqUAs") # can change the default uid later
-    quote = request.form.get("quote", "NOT FILLED OUT")
-    quote_author = request.form.get("quote_author", "NOT FILLED OUT")
-    context = request.form.get("context", "NOT FILLED OUT")
-    add_post(user_id, quote, quote_author, context)
-    return redirect("/")
