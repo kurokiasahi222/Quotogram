94bceda9b447c301b919ecf53893f9524b57a50c
Jason Woitalla <jason.woitalla@gmail.com>
2023-03-11 08:06:14 -0600

    L System Fractal Tree, profile page placement

--- static/javascript/profile_page.js
+++ static/javascript/profile_page.js
@@ -1,5 +1,5 @@
 // Pan Zoom for profile page
-let quoteWrapper = document.querySelector('.profile-posts-wrapper');
+var quoteWrapper = document.querySelector('.profile-posts-wrapper');
 var instance = panzoom(quoteWrapper, {
     maxZoom: 2,
     minZoom: 0.25,
@@ -22,77 +22,15 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-// Setup force directed graph on the posts
-let posts = document.querySelectorAll('.quote-wrapper');
-
-// // Create links between posts by date posted
-// let links = [];
-// for (let i = 0; i < posts.length; i++) {
-//     for (let j = i + 1; j < posts.length; j++) {
-//         let post1 = posts[i];
-//         let post2 = posts[j];
-//         let post1Date = new Date(post1.querySelector('.quote-footer-date-text').innerHTML.trim());
-//         let post2Date = new Date(post2.querySelector('.quote-footer-date-text').innerHTML.trim());
-//         let diff = Math.abs(post1Date - post2Date);
-//         if (diff < (1000 * 60 * 60 * 24)+1) { // 1 day difference
-//             links.push({source: post1, target: post2});
-//         }
-//     }
-// }
-
-// center = {x: 1500, y: 1000};
-
-// // repeal stronger in the x direction
-// const simulation = d3.forceSimulation(posts)
-//     .force("charge", d3.forceManyBody().strength(-100))
-//     .force("center", d3.forceCenter(center.x, center.y))
-//     .force("collide", d3.forceCollide().radius(250))
-//     //.force("link", d3.forceLink().links(links).distance(100))
-
-// posts[0].fx = center.x;
-// posts[0].fy = center.y;
-
-// d3.selectAll(posts)
-//     .data(posts)
-//     .style("left", function(d) { return d.x + "px"; })
-//     .style("top", function(d) { return d.y + "px"; })
-//     .call(d3.drag()
-//         .on("start", dragStarted)
-//         .on("drag", dragged)
-//         .on("end", dragEnded));
-
-// function dragStarted(d) {
-//     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-//     d.fx = d.x;
-//     d.fy = d.y;
-// }
-
-// function dragged(d) {
-//     d.fx = d3.event.x;
-//     d.fy = d3.event.y;
-// }
-
-// function dragEnded(d) {
-//     if (!d3.event.active) simulation.alphaTarget(0);
-//     d.fx = null;
-//     d.fy = null;
-// }
-
-// simulation.on("tick", function() {
-//     d3.selectAll(posts)
-//         .style("left", function(d) { return d.x + "px"; })
-//         .style("top", function(d) { return d.y + "px"; });
-// });
-
 // L System Fractal Tree to calculate the position of the quotes
+let posts = document.querySelectorAll('.quote-wrapper');
 let axiom = "F";
 let rules = [
-    {a: "F", b: "F[+FF][-FF]F[-F][+F]F"},
+    {a: "F", b: "FF+[+F-F-F]-[-F+F+F]"},
 ]
-let angle = 36;
+let angle = 22;
 let iterations = 3;
-let len = 500;
-let offset = {x: 250, y: 50};
+let len = 700;
 
 function generateTree(sentence) {
     let nextSentence = "";
@@ -121,20 +59,22 @@ function calculatePositions() {
     }
     console.log(sentence);
 
-    let pos = {x: 0, y: 0};
+    let pos = {x: -len, y: 0};
     let stack = [];
     let positions = [];
-    let curAngle = 90;
-    positions.push(pos);
+    let curAngle = 0;
 
     for (let i = 0; i < sentence.length; i++) {
         let current = sentence.charAt(i);
         if (current == "F" || current == "X") {
             let newPos = {
-                x: pos.x + len * Math.cos(curAngle * Math.PI / 180) + offset.x,
-                y: pos.y + len * Math.sin(curAngle * Math.PI / 180) + offset.y
+                x: pos.x + (len * Math.cos(curAngle * Math.PI / 180)),
+                y: pos.y + (len * Math.sin(curAngle * Math.PI / 180))
             };
             positions.push(newPos);
+            if(positions.length == posts.length) {
+                break;
+            }
             pos = newPos;
         } else if (current == "+") {
             curAngle += angle;
@@ -145,15 +85,53 @@ function calculatePositions() {
         } else if (current == "]") {
             let state = stack.pop();
             pos = state.pos;
-            angle = state.angle;
+            curAngle = state.angle;
         }
     }
     return positions;
 }
 
-let center = {x: 1500, y: 50};
+let center = {x: 30, y: 700};
 let positions = calculatePositions();
 for(let i = 0; i < posts.length; i++) {
-    posts[i].style.left = positions[i].x + center.x + "px";
-    posts[i].style.top = positions[i].y + center.y + "px";
+    posts[i].style.transform = "translate(" + (positions[i].x + center.x) + "px, " + (positions[i].y + center.y) + "px)";
 }
+
+var isDragging = false;
+document.addEventListener("mousedown", function(event) {
+    var draggable = event.target.closest(".quote-pin-handle");
+
+    if (draggable) {
+        if(!isDragging) {
+            isDragging = true;
+            instance.pause();
+
+            let post = draggable.closest(".quote-wrapper");
+            let wrapperPos = quoteWrapper.getBoundingClientRect();
+            let postPos = post.getBoundingClientRect();
+
+            let mousePos = {
+                x: event.clientX - postPos.left + wrapperPos.left - instance.getTransform().x, 
+                y: event.clientY - postPos.top + wrapperPos.top - instance.getTransform().y
+            };
+            post.style.zIndex = 1000;
+            
+            let drag = function(event) {
+                let translatedX = (event.clientX - mousePos.x - instance.getTransform().x) / instance.getTransform().scale;
+                let translatedY = (event.clientY - mousePos.y - instance.getTransform().y) / instance.getTransform().scale;
+                post.style.transform = "translate(" + translatedX + "px, " + translatedY + "px)";
+            }
+
+            let drop = function(event) {
+                isDragging = false;
+                instance.resume();
+                post.style.zIndex = 0;
+                document.removeEventListener("mousemove", drag);
+                document.removeEventListener("mouseup", drop);
+            }
+
+            document.addEventListener("mousemove", drag);
+            document.addEventListener("mouseup", drop);
+        }
+    }
+});
