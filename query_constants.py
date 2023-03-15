b1e410ec6653e7b8d57b66054b269ffaacdf1891
Jason Woitalla <jason.woitalla@gmail.com>
2023-03-15 10:22:06 -0500

    emergency fix

--- query_constants.py
+++ query_constants.py
@@ -116,7 +116,7 @@ POSTS_NOT_LOGGED_IN = POST_LIKES_HEADER + """
 SELECT p.post_id,p.user_id,p.quote,p.quote_author,p.context, p.creation_time, l.num_likes, false as quote_added, u.profile_image, u.username
 FROM post p, likes_for_all_posts_id l, users u
 WHERE p.post_id = l.post_id AND p.user_id = u.user_id
-ORDER BY p.creation_time DESC, p.num_likes DESC, p.post_id DESC
+ORDER BY p.creation_time DESC, l.num_likes DESC, p.post_id DESC
 LIMIT %s OFFSET %s"""
 
 POSTS_LOGGED_IN = POST_LIKES_HEADER + """,
